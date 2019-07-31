04d61d7d0d1fce141825ff620b141af439981908
junhojohn <junhojohn@gmail.com>
2019-07-31 13:12:08 +0900

    initial commit

---
+++ Register.php
@@ -0,0 +1,17 @@
+<?php
+	$con = mysqli_connect("localhost", "geniemarketdb", "kk753030!", "geniemarketdb");
+	
+	$productName = $_POST["productName"];
+	$productPrice = $_POST["productPrice"];
+	$productDescription = $_POST["productDescription"];
+	$productItemCnt = $_POST["productItemCnt"];
+	
+	$statement = mysqli_prepare($con, "INSERT INTO PRODUCTINFO VALUES (?,?,?,?);");
+	mysqli_stmt_bind_param($statement, "sssi", $productName, $productPrice, $productItemCnt, $productDescription);
+	mysqli_stmt_execute($statement);
+	
+	$response = array();
+	$response["success"] = true;
+	
+	echo json_encode($response);
+?>
