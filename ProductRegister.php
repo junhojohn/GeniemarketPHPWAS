e99b4753f135553ed0754d5c2a29d858c49e8d0a
junhojohn <junhojohn@gmail.com>
2019-08-07 13:39:41 +0900

    execute 2 insert queries with only one server file.(ProductRegister.php)

--- ProductRegister.php
+++ ProductRegister.php
@@ -6,9 +6,14 @@
 	$productItemCnt = $_POST["productItemCnt"];
 	$productDescription = $_POST["productDescription"];
 	$sellerID = $_POST["sellerID"];
+	$productImage1 = $_POST["productImage1"];
 	
 	$statement = mysqli_prepare($con, "INSERT INTO PRODUCTINFO VALUES (?,?,?,?,?);");
 	mysqli_stmt_bind_param($statement, "sssss", $productName, $productPrice, $productItemCnt, $productDescription, $sellerID);
+	mysqli_stmt_execute($statement);
+	
+	$statement = mysqli_prepare($con, "INSERT INTO IMAGEPATHINFO VALUES (?,?,?);");
+	mysqli_stmt_bind_param($statement, "sss", $productImage1, $productName, $sellerID);
 	mysqli_stmt_execute($statement);
 	
 	$response = array();
