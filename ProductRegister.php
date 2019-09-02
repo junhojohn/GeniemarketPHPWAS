13bc25bca93b92603b0db1879e3f009d2c907f12
junhojohn <junhojohn@gmail.com>
2019-09-02 21:33:08 +0900

    add category for product info table

--- ProductRegister.php
+++ ProductRegister.php
@@ -5,11 +5,16 @@
 	$productPrice = $_POST["productPrice"];
 	$productItemCnt = $_POST["productItemCnt"];
 	$productDescription = $_POST["productDescription"];
+	$bigCategory = $_POST["bigCategory"];
 	$sellerID = $_POST["sellerID"];
 	$productImage1 = $_POST["productImage1"];
+
+	$statement = mysqli_prepare($con, "INSERT INTO SELLERINFO VALUES (?,?,?,?,?,?);");
+	mysqli_stmt_bind_param($statement, "ssssss", $sellerID, $sellerID, $sellerID, $sellerID, $sellerID, $sellerID);
+	mysqli_stmt_execute($statement);
 	
-	$statement = mysqli_prepare($con, "INSERT INTO PRODUCTINFO VALUES (?,?,?,?,?);");
-	mysqli_stmt_bind_param($statement, "sssss", $productName, $productPrice, $productItemCnt, $productDescription, $sellerID);
+	$statement = mysqli_prepare($con, "INSERT INTO PRODUCTINFO VALUES (?,?,?,?,?,?);");
+	mysqli_stmt_bind_param($statement, "ssssss", $productName, $productPrice, $productItemCnt, $productDescription, $bigCategory, $sellerID);
 	mysqli_stmt_execute($statement);
 	
 	$statement = mysqli_prepare($con, "INSERT INTO IMAGEPATHINFO VALUES (?,?,?);");
